8998c2f1007dc7ef0ff1e9d73547e92c24f727c1
Teerapath-P101 <teerapath64242@gmail.com>
2021-08-19 06:24:15 +0700

    Commit

--- commit.py
+++ commit.py
@@ -2,6 +2,6 @@ import os
 os.system("git init")
 os.system("git add .")
 os.system("git commit -m 'Commit'")
-os.sytem("git remote add origin 'https://github.com/Teerapath-P101/TeleBot.git'")
-os.sytem("git push -u origin main")
+os.system("git remote add origin 'https://github.com/Teerapath-P101/TeleBot.git'")
+os.system("git push -u origin main")
 
